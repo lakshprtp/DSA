e2f3d00d52cd4a769782197816632431138dff9d
lakshprtp <lakshyavasishtha@gmail.com>
2026-09-08 14:38:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.51%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int t=0;
+        if (n<999) return 0;
+        
+        else{
+            t=n-999;
+            
+        }
+        return t;
+        
+    }
+};
